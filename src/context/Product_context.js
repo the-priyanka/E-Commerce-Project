f27f8e0d5e274bbdce349023d12d97693fcd7928
Priyanka <original.manaria@gmail.com>
2022-03-30 10:58:12 +0530

    Products Context Setup

--- src/context/Product_context.js
+++ src/context/Product_context.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import React, { useContext, useEffect, useReducer } from "react";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+} from "react";
 import reducer from "../reducers/products_reducer";
 import { products_url as url } from "../utils/constants";
 import {
@@ -13,13 +18,27 @@ import {
   GET_SINGLE_PRODUCT_ERROR,
 } from "../actions";
 
-const initialState = {};
+const initialState = {
+  isSidebarOpen: false,
+};
 
-const ProductsContext = React.createContext();
+const ProductsContext = createContext();
 
 export const ProductsProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const openSidebar = () => {
+    dispatch({ type: SIDEBAR_OPEN });
+  };
+
+  const closeSidebar = () => {
+    dispatch({ type: SIDEBAR_CLOSE });
+  };
+
   return (
-    <ProductsContext.Provider value="products context">
+    <ProductsContext.Provider
+      value={{ ...state, openSidebar, closeSidebar }}
+    >
       {children}
     </ProductsContext.Provider>
   );
