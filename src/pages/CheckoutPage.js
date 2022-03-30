0695832d76c273c87e0ac1cedf3424b40fd54e6f
Priyanka <original.manaria@gmail.com>
2022-03-30 12:06:14 +0530

    About, Checkout Page

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
 
 const CheckoutPage = () => {
-  return <div>CheckoutPage</div>;
+  return (
+    <main>
+      <PageHero title="checkout" />
+      <Wrapper className="page">
+        <h1>checkout here</h1>
+      </Wrapper>
+    </main>
+  );
 };
+
+const Wrapper = styled.div``;
 
 export default CheckoutPage;
