b04a07cd77d2e7c3531589748d8e1f1057d03f0e
Priyanka <original.manaria@gmail.com>
2022-03-31 11:18:15 +0530

    Featured Products

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="section section-center">
+      <div className="loading"></div>
+    </div>
+  );
 };
 
 export default Loading;
