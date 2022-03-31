af02b284c87b31bd07127ac0476a3f82898b10a0
Priyanka <original.manaria@gmail.com>
2022-03-31 10:47:01 +0530

    Fetch Products Overview

--- src/context/Product_context.js
+++ src/context/Product_context.js
@@ -20,6 +20,10 @@ import {
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 };
 
 const ProductsContext = createContext();
@@ -36,8 +40,14 @@ export const ProductsProvider = ({ children }) => {
   };
 
   const fetchProduct = async (url) => {
-    const response = await axios.get(url);
-    console.log(response);
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
+    }
   };
 
   useEffect(() => {
