c8b9caf086b8bc1a491d5be565419b490e3c247a
Priyanka <original.manaria@gmail.com>
2022-04-02 12:14:03 +0530

    Products Page - List View

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -20,6 +20,7 @@ import { useProductsContext } from "./products_context";
 const initialState = {
   filtered_products: [],
   all_products: [],
+  grid_view: false,
 };
 
 const FilterContext = createContext();
